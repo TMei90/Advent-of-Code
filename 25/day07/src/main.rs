beb36a76cbdeb1f4083c662ac36335e341cad478
TMei90 <timm-meiwes@gmx.de>
2026-04-05 01:19:42 +0200

    day07 Part1

--- 25/day07/src/main.rs
+++ 25/day07/src/main.rs
@@ -12,5 +12,40 @@ fn main() {
         input = EXAMPLE;
     }
     let input = fs::read_to_string(path::Path::new(input)).expect("Failed to read file");
-    let mut lines = input.lines().collect::<Vec<&str>>();
+    let lines = input.lines().collect::<Vec<&str>>();
+
+    let mut map: Vec<Vec<char>> = Vec::new();
+    for line in lines {
+        let chars = line.chars().collect::<Vec<char>>();
+        map.push(chars);
+    }
+
+    // for i in 0..map.len() {
+    //     println!("{:?}", map[i]);
+    // }
+    // println!();
+
+    let mut splits = 0;
+    for row in 0..map.len() {
+        for col in 0..map[0].len() {
+            if map[row][col] == 'S' {
+                map[row + 1][col] = '|';
+            } else if map[row][col] == '^' && map[row - 1][col] == '|' {
+                splits += 1;
+                if col > 0 && map[row][col - 1] == '.' {
+                    map[row][col - 1] = '|';
+                }
+                if col < map[0].len() - 1 && map[row][col + 1] == '.' {
+                    map[row][col + 1] = '|';
+                }
+            } else if row > 0 && map[row - 1][col] == '|' && map[row][col] == '.' {
+                map[row][col] = '|';
+            }
+        }
+        // for i in 0..map.len() {
+        //     println!("{:?}", map[i]);
+        // }
+        // println!()
+    }
+    print!("Splits: {}", splits);
 }
