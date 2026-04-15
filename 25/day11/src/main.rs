d09d980c59a169847912d840921b2f7c749aff9b
TMei90 <timm-meiwes@gmx.de>
2026-04-15 23:18:36 +0200

    day11 Part1

--- 25/day11/src/main.rs
+++ 25/day11/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
-use std::{collections::HashSet, env, fs, path, result};
+use std::collections::{HashMap, HashSet};
+use std::os::windows::process;
+use std::{env, fs, path, result};
 
 const EXAMPLE: &str = "example_input.txt";
 const REAL: &str = "input.txt";
@@ -14,4 +16,24 @@ fn main() {
     }
     let input = fs::read_to_string(path::Path::new(input)).expect("Failed to read file");
     let lines = input.lines().collect::<Vec<&str>>();
+    let map: HashMap<String, String> = lines
+        .iter()
+        .map(|line| {
+            let line = line.split(":").collect::<Vec<&str>>();
+            (line[0].trim().to_string(), line[1].trim().to_string())
+        })
+        .collect();
+
+    let mut current_vec: Vec<&str> = map.get("you").unwrap().split_whitespace().collect();
+    println!("{:?}", current_vec);
+    let mut path = 0;
+    while current_vec.len() > 0 {
+        let curren_str = current_vec.remove(0);
+        if curren_str == "out" {
+            path += 1;
+        } else {
+            current_vec.extend(map.get(curren_str).unwrap().split_whitespace());
+        }
+    }
+    println!("Path: {}", path);
 }
