0ca0e0bb49b61d2f1a2e897060620817add752e2
TMei90 <timm-meiwes@gmx.de>
2026-04-16 01:10:42 +0200

    setup day12

---
+++ 25/day12/src/main.rs
@@ -0,0 +1,18 @@
+#![allow(unused)]
+use std::collections::{HashMap, HashSet};
+use std::{env, fs, path, result};
+
+const EXAMPLE: &str = "example_input.txt";
+const REAL: &str = "input.txt";
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let real_input = &args[1];
+    let input: &str;
+    if real_input == "REAL" {
+        input = REAL;
+    } else {
+        input = EXAMPLE;
+    }
+    let input = fs::read_to_string(path::Path::new(input)).expect("Failed to read file");
+    let lines = input.lines().collect::<Vec<&str>>();
+}
